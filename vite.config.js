b95efe5077863d529d5bdda843caaec73dbc4f22
Bari <charlespalatino@gmail.com>
2025-09-24 08:48:20 -0400

    ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping",
   plugins: [react(), tailwindcss()],
 })
