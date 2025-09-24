22e21dac8bf95489a98dd3714cc1ad20790d66f8
Bari <charlespalatino@gmail.com>
2025-09-24 11:47:12 +0800

    fix: landing page design

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -3,16 +3,24 @@ import './AboutUs.css';
 
 function AboutUs() {
   return (
-    <div>
-      {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
-      </p>
-      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-    </div>
+      <div className='w-auto px-10 lg:w-230 lg:px-auto text-white mx-auto text-center'>
+        <p className='text-lg'>
+          At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every plant enthusiast.
+        </p>
+
+        <br/>
+
+        <p className='text-lg'>
+          Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your home or office.
+        </p>
+
+         <br/>
+         <br/>
+        
+        <p className='italic text-sm'>
+          Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the beauty of nature right at your doorstep.
+        </p>
+      </div>
   );
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,26 @@ function App() {
     <div className="app-container">
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
-        <div className="backdrop-blur-sm bg-gray/50 h-250">
-         <div className="landing_content">
-         <h1 className='text-white text-4xl'>Welcome To Mobile Legends</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
+        <div className="backdrop-blur-md bg-gradient-to-b from-black/40 to-black/70 h-250">
+         <div className="flex justify-center">
+
+          <div className="text-center mt-20">
+            <h1 className="text-[#4caf50] text-5xl lg:text-6xl drop-shadow-lg mb-10">Welcome To Paradise Nursery</h1>
+            <hr className="h-1 w-1/3 mx-auto mb-10 mt-4 bg-gradient-to-r from-[#4caf50] to-sky-400 border-0 rounded" />
+            <p className="text-3xl text-sky-300 font-semibold mb-5">Where Green Meets Serenity</p>
+          </div>
+          
          </div>
-          <div className="aboutus_container">
-          <AboutUs/>
+          <div>
+            <AboutUs/>
+          </div>
+
+          <div className='flex justify-center'>
+            <button 
+              className="bg-[#4caf50] py-2.5 px-5 rounded-sm text-white cursor-pointer hover:opacity-85 transition-all duration-400 mt-10" 
+              onClick={handleGetStartedClick}>
+              Get Started
+            </button>
           </div>
           </div>
 
